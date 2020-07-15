769bb8ee3e4b009504757a8315b472174e33f9f1
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-15 01:19:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  original_array = string.split('')
+  original_array = string.split("")
   return_array = []
   original_array.each do|string|
-    return_array << string.reverse
+    return_array = string.reverse
   end
-  return_array.join('')
+  return_array.join("")
 end
