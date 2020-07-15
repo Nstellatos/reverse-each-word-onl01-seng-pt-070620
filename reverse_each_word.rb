1f9dc1c3e40be4639f2066c7d847f4ef7eaead43
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-15 01:30:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   new_sentence = []
-  string.sentence.each do |reverse_words|
+  string.split.each do |reverse_words|
     new_sentence << reverse_words.reverse
   end
   new_sentence
